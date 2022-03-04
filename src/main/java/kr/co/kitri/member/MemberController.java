362c47473942ce118086cf979a71cda5bbc1b61f
kitri <kitri@DESKTOP-4G1D7HS>
2022-03-04 16:04:33 +0900

    한글 에러

--- src/main/java/kr/co/kitri/member/MemberController.java
+++ src/main/java/kr/co/kitri/member/MemberController.java
@@ -29,7 +29,7 @@ public class MemberController {
 	
 	@RequestMapping("/signin-4")
 	public String singIn4() {
-		System.out.println("잘올라가지나!!!!");
+		System.out.println("etstetst");
 		return "views/member/sign-in";
 	}
 	
