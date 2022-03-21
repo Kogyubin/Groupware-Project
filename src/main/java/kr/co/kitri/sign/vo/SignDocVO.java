0910bc34945379ee16c6e3be50b2ba7144a86199
kitri <kitri@DESKTOP-4G1D7HS>
2022-03-21 14:54:05 +0900

    메인화면 커밋

---
+++ src/main/java/kr/co/kitri/sign/vo/SignDocVO.java
@@ -0,0 +1,92 @@
+package kr.co.kitri.sign.vo;
+
+
+
+public class SignDocVO {
+	private String docno;
+	private String doctitle;
+	private String doccontent;
+	private String docstatus;
+	private String deletetag;
+	private String docstep;
+	private String signpath;
+	private String dtno;
+	private String empno;
+	private String deptname;
+	private String docregdate;
+	private String docupdate;
+	public String getDocno() {
+		return docno;
+	}
+	public void setDocno(String docno) {
+		this.docno = docno;
+	}
+	public String getDoctitle() {
+		return doctitle;
+	}
+	public void setDoctitle(String doctitle) {
+		this.doctitle = doctitle;
+	}
+	public String getDoccontent() {
+		return doccontent;
+	}
+	public void setDoccontent(String doccontent) {
+		this.doccontent = doccontent;
+	}
+	public String getDocstatus() {
+		return docstatus;
+	}
+	public void setDocstatus(String docstatus) {
+		this.docstatus = docstatus;
+	}
+	public String getDeletetag() {
+		return deletetag;
+	}
+	public void setDeletetag(String deletetag) {
+		this.deletetag = deletetag;
+	}
+	public String getDocstep() {
+		return docstep;
+	}
+	public void setDocstep(String docstep) {
+		this.docstep = docstep;
+	}
+	public String getSignpath() {
+		return signpath;
+	}
+	public void setSignpath(String signpath) {
+		this.signpath = signpath;
+	}
+	public String getDtno() {
+		return dtno;
+	}
+	public void setDtno(String dtno) {
+		this.dtno = dtno;
+	}
+	public String getEmpno() {
+		return empno;
+	}
+	public void setEmpno(String empno) {
+		this.empno = empno;
+	}
+	public String getDeptname() {
+		return deptname;
+	}
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+	public String getDocregdate() {
+		return docregdate;
+	}
+	public void setDocregdate(String docregdate) {
+		this.docregdate = docregdate;
+	}
+	public String getDocupdate() {
+		return docupdate;
+	}
+	public void setDocupdate(String docupdate) {
+		this.docupdate = docupdate;
+	}
+	
+	
+}
