fdb9491e26471e568d84b39fc702e79fd5ca16d0
kitri <kitri@DESKTOP-4G1D7HS>
2022-03-23 16:53:06 +0900

    user svc searchVO pageVO 커밋

---
+++ src/main/java/kr/co/kitri/admin/organ/UserSvc.java
@@ -0,0 +1,60 @@
+package kr.co.kitri.admin.organ;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.kitri.common.SearchVO;
+
+
+@Service
+public class UserSvc {
+
+    @Autowired
+    private SqlSessionTemplate sqlSession;    
+        
+    public List<?> selectUserList(String param) {
+        return sqlSession.selectList("selectUserList", param);
+    }
+    
+    public List<?> selectUserListWithDept(SearchVO param) {
+        return sqlSession.selectList("selectUserListWithDept", param);
+    }
+    
+//    /**
+//     * 사용자 저장.     
+//     */
+//    public void insertUser(UserVO param) {
+//        if (param.getUserno() == null || "".equals(param.getUserno())) {
+//             sqlSession.insert("insertUser", param);
+//        } else {
+//            sqlSession.insert("updateUser", param);
+//        }
+//    }
+//    
+//    public String selectUserID(String param) {
+//        return sqlSession.selectOne("selectUserID", param);
+//    }
+//
+//    public UserVO selectUserOne(String param) {
+//        return sqlSession.selectOne("selectUserOne", param);
+//    }
+//    
+//    public void deleteUser(String param) {
+//        sqlSession.delete("deleteUser", param);
+//    }
+//
+//    /*
+//     * 사용자 
+//     */
+//    public void updateUserByMe(UserVO param) {
+//        sqlSession.delete("updateUserByMe", param);
+//    }
+//    
+//    public void updateUserPassword(UserVO param) {
+//        sqlSession.delete("updateUserPassword", param);
+//    }
+
+}
